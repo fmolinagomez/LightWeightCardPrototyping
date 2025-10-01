dbac5aeba6aa0e9b841b13dadf374eee24b6e785
fmolinagomez <33826980+fmolinagomez@users.noreply.github.com>
2025-10-01 11:44:38 +0200

    Add CLI support for individual card PNG exports

--- LWCProto.py
+++ LWCProto.py
@@ -1,9 +1,7 @@
 #! /usr/bin/env python3
 import csv
-import json
 import os
-import pathlib
-import sys
+import re
 import numpy as np
 
 import cairo
@@ -37,6 +35,7 @@ parser.add_argument('-c', '--cards', type=extant_file, help='json file containin
 parser.add_argument('-i', '--images', help='Add images to cards', action='store_true')
 parser.add_argument('-r', '--rgb', help='Update layout card border colour with given R,G,B, only works with default layout', nargs=3, type=int)
 parser.add_argument('-l', '--layout', help='Use a different layout than default', type=extant_file, metavar="FILE")
+parser.add_argument('--single-card', help='Render each card as an individual 63x85mm PNG at 300 DPI', action='store_true')
 
 
 args = parser.parse_args()
@@ -45,10 +44,18 @@ handle_images = args.images
 modify_layout = args.rgb
 deck_file = args.deck
 cards_file = args.cards
+single_card_mode = args.single_card
 #deck_file = './example_deck.csv'
 deck_name = os.path.basename(deck_file)[:-4]
 nameList = []
 list_copy = []
+
+if handle_images or (modify_layout is not None):
+    from add_images import BaseImage
+
+if handle_images:
+    from add_images import addImage
+    from add_images import processImage
 
 with open(deck_file, encoding='utf-8') as csvFile:
     reader = csv.reader(csvFile)
@@ -67,53 +74,90 @@ if not os.path.exists('decks'):
 if not os.path.exists(os.path.join('decks',deck_name)):
     os.mkdir(os.path.join('decks',deck_name))
 
-for page_number in range(len(pageList)):
-    print(f'Page {page_number}:')
-    page = pageList[page_number]
-    surf = layout.getSurface()
-    ctx = cairo.Context(surf)
+if single_card_mode:
+    cards_output_dir = os.path.join('decks', deck_name, 'cards')
+    os.makedirs(cards_output_dir, exist_ok=True)
+    single_dpi = layout.SINGLE_CARD_DPI
 
-    for i in range(len(page)):
-        card = page[i]
-        cardPos = (i % 3, i // 3)
-        print(cardPos)
-        print(card)
-        mat = layout.getMatrix(*cardPos, surf)
-        ctx.set_matrix(mat)
+    def _slugify(value: str) -> str:
+        value = value.strip()
+        value = re.sub(r'\s+', '_', value)
+        value = re.sub(r'[^A-Za-z0-9_-]', '', value)
+        return value or 'card'
+
+    for index, card in enumerate(cardList):
+        print(f'Card {index}: {card}')
+        surf = layout.get_single_card_surface(single_dpi)
+        ctx = cairo.Context(surf)
+        ctx.set_matrix(layout.get_single_card_matrix(single_dpi))
         drawCard(card, ctx)
 
-    surf.write_to_png(f'decks/{deck_name}/{deck_name}_p{page_number}.png')
+        card_filename = f"{index:03d}_{_slugify(card.nameStr)}.png"
+        output_path = os.path.join(cards_output_dir, card_filename)
+        surf.write_to_png(output_path)
 
-    from add_images import BaseImage
-    from add_images import addImage
-    from add_images import processImage
-    from PIL import Image
+        if handle_images:
+            processImage(card, deck_name, dpi=single_dpi)
+            baseImage = BaseImage(output_path)
+            updated_image = addImage(card, baseImage, deck_name, dpi=single_dpi)
+            baseImage.update(updated_image)
+            baseImage.save(output_path)
+else:
+    for page_number in range(len(pageList)):
+        print(f'Page {page_number}:')
+        page = pageList[page_number]
+        surf = layout.getSurface()
+        ctx = cairo.Context(surf)
 
-    if (modify_layout is not None):
-        baseImage = BaseImage(f'decks/{deck_name}/{deck_name}_p{page_number}.png')
-        temp = baseImage.baseImage.convert('RGBA')
-        data = np.array(temp)
-        red, green, blue, alpha = data.T 
-        for i in range(0,63):
-            white_areas = (red == 190+i) & (blue == 190+i) & (green == 190+i)
-            data[..., :-1][white_areas.T] = (modify_layout[0], modify_layout[1], modify_layout[2])
-        baseImage.update(Image.fromarray(data))
-        baseImage.save(f'decks/{deck_name}/{deck_name}_p{page_number}.png')
-
-
-    #import pdb;pdb.set_trace()
-    if (handle_images):
-
-        if not os.path.exists(os.path.join('decks',deck_name,'images')):
-            os.mkdir(os.path.join('decks',deck_name,'images'))
-        #open the previous png to add the images
-        baseImage = BaseImage(f'decks/{deck_name}/{deck_name}_p{page_number}.png')
-        for i in range (len(page)):
+        for i in range(len(page)):
             card = page[i]
             cardPos = (i % 3, i // 3)
-            processImage(card,deck_name)
-            baseImage.update(addImage(card,baseImage,deck_name, cardPos))
-        baseImage.save(f'decks/{deck_name}/{deck_name}_p{page_number}.png')
+            print(cardPos)
+            print(card)
+            mat = layout.getMatrix(*cardPos, surf)
+            ctx.set_matrix(mat)
+            drawCard(card, ctx)
+
+        output_path = f'decks/{deck_name}/{deck_name}_p{page_number}.png'
+        surf.write_to_png(output_path)
+
+        if (modify_layout is not None):
+            from PIL import Image
+
+            baseImage = BaseImage(output_path)
+            temp = baseImage.baseImage.convert('RGBA')
+            data = np.array(temp)
+            red, green, blue, alpha = data.T
+            for i in range(0,63):
+                white_areas = (red == 190+i) & (blue == 190+i) & (green == 190+i)
+                data[..., :-1][white_areas.T] = (modify_layout[0], modify_layout[1], modify_layout[2])
+            baseImage.update(Image.fromarray(data))
+            baseImage.save(output_path)
+
+
+        #import pdb;pdb.set_trace()
+        if (handle_images):
+            page_dpi = layout.get_surface_dpi(surf)
+            baseImage = BaseImage(output_path)
+            for i in range (len(page)):
+                card = page[i]
+                cardPos = (i % 3, i // 3)
+                card_origin_mm = layout.get_card_origin_mm(cardPos)
+                image_position_mm = (
+                    card_origin_mm[0] + layout.ART_OFFSET_MM[0],
+                    card_origin_mm[1] + layout.ART_OFFSET_MM[1],
+                )
+                processImage(card,deck_name, dpi=page_dpi)
+                baseImage.update(
+                    addImage(
+                        card,
+                        baseImage,
+                        deck_name,
+                        position_mm=image_position_mm,
+                        dpi=page_dpi,
+                    )
+                )
+            baseImage.save(output_path)
 
 
 with open(f'decks/{deck_name}/{deck_name}.csv', 'w') as deck_copy:
